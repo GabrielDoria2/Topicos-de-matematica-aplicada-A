a4b32f3da8fa16d1faa7c3f0331c74b8e3052f76
GbarielDoria <pedrosadoriagabriel@gmail.com>
2019-04-05 20:30:37 -0300

    Lista 2

--- Lista 2/soma_dos_termos_impares_fibonacci.py
+++ Lista 2/soma_dos_termos_impares_fibonacci.py
@@ -1,8 +1,10 @@
-# Tópicos de matemática aplicada A
-# Rio de Janeiro, 25 de março de 2019
-# Gabriel Pedrosa Dória
-# DRE: 119065076
-
+'''
+UFRJ - Universidade Federal do Rio de Janeiro
+IM -Instituto de Matemática
+DMA - Departamento de Matemática Aplicada
+Aluno: Gabriel Pedrosa Dória
+ DRE 119065076
+'''
 def fibonacci(n):
     if(n==1):
         return 0
