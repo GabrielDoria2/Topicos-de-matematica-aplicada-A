60140b6ef22729e90e6823f4ecfb7ad74b5e52d7
GbarielDoria <pedrosadoriagabriel@gmail.com>
2019-04-05 20:28:21 -0300

    Update multiplos.py

--- Lista 1/multiplos.py
+++ Lista 1/multiplos.py
@@ -1,3 +1,10 @@
+'''
+UFRJ - Universidade Federal do Rio de Janeiro
+IM -Instituto de Matemática
+DMA - Departamento de Matemática Aplicada
+Aluno: Gabriel Pedrosa Dória
+ DRE 119065076
+'''
 n = int (input('insira n: '))
 x = int (input('insira x: '))
 y = int (input('insira y: '))
